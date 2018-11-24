e10c8d47c6e8b3130c13106b6f86c3becadee17f
shotaro-ooe <shoooe97@gmail.com>
2018-11-24 11:54:56 +0900

    TestCalculation2 実行

--- test_sample04.py
+++ test_sample04.py
@@ -6,8 +6,11 @@ def add(number1, number2):
 
 
 class TestCalculation(unittest.TestCase):
-    def test_二つの整数の和を計算できる(self):
+    def test_二つの整数の和を計算できる_3タス4は7(self):
         self.assertEqual(7, add(3, 4))
+
+    def test_二つの整数の和を計算できる_2タス3は5(self):
+        self.assertEqual(5,add(2,3))
 
 
 if __name__ == "__main__":
